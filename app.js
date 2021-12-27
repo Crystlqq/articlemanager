219e72e30a3b5b158075d723171ddbc7341e3a2b
handsomezyx <2312534187@qq.com>
2021-12-27 03:44:58 -0800

    work4

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
+var express = require('express');
 var session = require('express-session')
 var articleRouter = require('./routes/article')
 //session的配置
+var app = express()
+app.set('view engine', 'ejs')//将ejs集成到express中
 app.use(session({
     secret:'qf project',
     resave:false,
@@ -10,14 +13,13 @@ app.use(session({
 
 app.use('article',articleRouter)
 //登录拦截
-app.get('*',function(req,res,next){
-    var username = req.session.username
-    var path =req.path
-    console.log('session',username)
-    if(path!='/login'&&path!='/regist'){
-        if(!username){
-        res.redirect('/login')
-     } 
-    }
-    next()
+app.get('/',function(req,res){
+    res.redirect('/login')
+})
+app.get('/login',function(req,res){
+    res.render('login.ejs')
+ })
+app.listen(3000, (err) => {
+    if(err) return console.log(err);
+    console.log('3000 open')
 })
